8bd7ce1e088e4d3531a8ab2c14759b53db1378e4
just.ernst <ernst.r.fanfan@gmail.com>
2021-07-31 14:47:28 -0400

    refactor main for updated Gui class

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 # SMYT STEM
 # Ernst R Fanfan
-# 7/21/2021
+# 7/30/2021
 
-import gui as g
-from operations import Operations
-
-# initializing
-EVENT_LIST = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "/", "-", "+", "x", "AC", "=", "."]
-
+from gui import Gui
 
 # init gui
-VIEW = None
-WINDOW = g.gui_init()
-background_processes = Operations()
+gui = Gui()
 
 # event loop
-while True:
-    event, values = WINDOW.read()
-    if g.is_closed(event):
-        break
-
-    # event processor
-    if event in EVENT_LIST:
-        update = background_processes.update_view(event)
-        VIEW = update if update is not None else VIEW
-        g.update_display(WINDOW, VIEW)
-
-WINDOW.close()
+while not gui.is_closed():
+    gui.read_click()
+    gui.update_display()
+else:
+    gui.close()
